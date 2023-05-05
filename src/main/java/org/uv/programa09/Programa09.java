80fa30fb54ba98a0e360f58e059e5e2646094b0a
Patidifusa <zs20006765@estudiantes.uv.mx>
2023-05-05 10:28:08 -0500

    inicializar

---
+++ src/main/java/org/uv/programa09/Programa09.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package org.uv.programa09;
+
+/**
+ *
+ * @author LilyE
+ */
+public class Programa09 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
